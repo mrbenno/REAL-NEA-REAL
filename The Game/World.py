7b0ef92162bb2dc17518b0e7c67c4b2fca070611
mrbenno <146442015+mrbenno@users.noreply.github.com>
2025-03-22 22:51:27 +0000

    Add files via upload

---
+++ The Game/World.py
@@ -0,0 +1,23 @@
+import pygame
+
+class World():
+    def __init__(self, data):
+        self.data = data
+        self.colour = '#283618'
+        self.tileList = []
+
+    def buildWorld(self, tileSize):
+        rowCount = 0
+        for row in self.data:
+            coloumnCount = 0
+            for tile in row:
+                if tile == 1:
+                    tile = pygame.Rect((tileSize * coloumnCount, tileSize * rowCount), (tileSize, tileSize))
+                    self.tileList.append(tile)
+                coloumnCount += 1
+            rowCount += 1
+        return self.tileList, tile
+    
+    def draw(self, screen):
+        for tile in self.tileList:
+            pygame.draw.rect(screen, self.colour, tile)
